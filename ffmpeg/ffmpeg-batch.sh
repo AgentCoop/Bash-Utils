70062973ba1ad3de13c805c973dd5a76bc0af086
AgentCoop <twinpeaks.agent.cooper@gmail.com>
2017-02-21 14:46:38 +0300

    ffmpeg-batch.sh

--- ffmpeg/ffmpeg-batch.sh
+++ ffmpeg/ffmpeg-batch.sh
@@ -3,6 +3,13 @@
 set -ex
 
 PADDING_FILTER="pad=ih*16/9:ih:(ow-iw)/2:(oh-ih)/2,scale="
+INPUT=
+INPUT_RES_Y=
+INPUT_REGEXP=
+INPUT_ASPECT_RATIO=
+OUTPUT_CRF="-crf 28"
+OUTPUT_SPEC=
+DRY_RUN=
 
 is_corrupted() {
     local input="$1"
@@ -15,6 +22,19 @@ is_corrupted() {
         return 1
     else
         return 0
+    fi
+}
+
+input_to_ouptut() {
+    local output=$(echo basename "$INPUT" | awk "match(\$0, \"$INPUT_REGEXP\", m) {printf \"$OUTPUT_SPEC\", m[1],m[2],m[3],m[4],m[5],m[6]}" \
+        | sed "s/\s/_/g" \
+        | sed "s/['\"]//g" \
+        | sed 's/\]//g' \
+        | sed 's/\[//g' \
+        | sed 's/&/_and_/g')
+
+    if [[ -z $output ]]; then
+        exit -1
     fi
 }
 
@@ -32,9 +52,7 @@ get_video_res_y() {
 }
 
 convert-240p() {
-    local input="$1"
-    local output="./videos/240p/${input}_240.mp4"
-    local ratio=$(get_aspect_ratio "$input")
+    local output="./videos/240p/${OUTPUT_PREFIX}_240.mp4"
 
     if is_corrupted "$output"; then
         rm -f "$output"
@@ -46,20 +64,22 @@ convert-240p() {
 
     [[ ! -d ./videos/240p ]] && mkdir -p ./videos/240p
 
-    if [[ $ratio != 16:9 ]]; then
+    if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
         local filter="-vf ${PADDING_FILTER}426:240"
     else
         local filter="-vf scale=426:240"
     fi
 
-    /usr/bin/ffmpeg -i "$input" -sn -movflags faststart -strict -2 $filter "$output"    
+    if [[ $DRY_RUN = true ]]; then
+        echo "INPUT: $INPUT, OUTPUT: $output"
+    else
+        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 $OUTPUT_CRF $filter "$output"    
+    fi
 }
 
 convert-360p() {
-    local input="$1"
-    local output="./videos/360p/${input}_360.mp4"
-    local ratio=$(get_aspect_ratio "$input")
-    
+    local output="./videos/360p/${OUTPUT_PREFIX}_360.mp4"
+
     if is_corrupted "$output"; then
         rm -f "$output"
     fi
@@ -70,26 +90,27 @@ convert-360p() {
 
     [[ ! -d ./videos/360p ]] && mkdir -p ./videos/360p
 
-    if [[ $ratio != 16:9 ]]; then
+    if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
         local filter="-vf ${PADDING_FILTER}640:360"
     else
         local filter="-vf scale=640:360"
     fi
 
-    /usr/bin/ffmpeg -i "$input" -sn -movflags faststart -strict -2 $filter "$output"    
+    if [[ $DRY_RUN = true ]]; then
+        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 $OUTPUT_CRF $filter "$output" 
+    else
+        echo "INPUT: $INPUT, OUTPUT: $output"
+    fi
 }
 
 convert-480p() {
-    local input="$1"
-    local output="./videos/480p/${input}_480.mp4"
-    local resy=$(get_video_res_y "$input")
-    local ratio=$(get_aspect_ratio "$input")
+    local output="./videos/480p/${OUTPUT_PREFIX}_480.mp4"
     
     if is_corrupted "$output"; then
         rm -f "$output"
     fi
 
-    if [[ -f $output ]] || [[ $resy -lt 480 ]]; then
+    if [[ -f $output ]] || [[ $INPUT_RES_Y -lt 480 ]]; then
         return
     fi
 
@@ -101,19 +122,21 @@ convert-480p() {
         local filter="-vf scale=854:480"
     fi
 
-    /usr/bin/ffmpeg -i "$input" -sn -movflags faststart -strict -2 $filter "$output"  
+    if [[ $DRY_RUN = true ]]; then
+        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 $OUTPUT_CRF $filter "$output" 
+    else
+        echo "INPUT: $INPUT, OUTPUT: $output"
+    fi 
 }
 
 convert-720p() {
-    local input="$1"
-    local output="./videos/720p/${input}_720.mp4"
-    local resy=$(get_video_res_y "$input")
+    local output="./videos/720p/${OUTPUT_PREFIX}_720.mp4"
 
     if is_corrupted "$output"; then
         rm -f "$output"
     fi
 
-    if [[ -f $output ]] || [[ $resy -lt 720 ]]; then
+    if [[ -f $output ]] || [[ $INPUT_RES_Y -lt 720 ]]; then
         return
     fi
 
@@ -125,12 +148,15 @@ convert-720p() {
         local filter="-vf scale=1280:720"
     fi
 
-    /usr/bin/ffmpeg -i "$input" -sn -movflags faststart -strict -2 $filter "$output" 
+    if [[ $DRY_RUN = true ]]; then
+        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 $OUTPUT_CRF $filter "$output"
+    else
+        echo "INPUT: $INPUT, OUTPUT: $output"
+    fi 
 }
 
 convert-1080p() {
-    local input="$1"
-    local output="./videos/1080p/${input}_1080.mp4"
+    local output="./videos/1080p/${OUTPUT_PREFIX}_1080.mp4"
 
     if is_corrupted "$output"; then
         rm -f "$output"
@@ -140,72 +166,105 @@ convert-1080p() {
         return
     fi
 
-    resy=$(get_video_res_y "$input")
     [[ ! -d ./videos/1080p ]] && mkdir -p ./videos/1080p
 
     if [[ $resy -gt 720 ]] && [[ ! $resy -eq 1080 ]]; then
-        /usr/bin/ffmpeg -i "$input" -sn -movflags faststart -strict -2 -crf 28 -vf scale=1920:1080 "$output"
+        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 -crf 28 -vf scale=1920:1080 "$output"
     elif [[ $resy -eq 1080 ]]; then
-        /usr/bin/ffmpeg -i "$input" -sn -movflags faststart -strict -2 -crf 28 "$output"
+        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 -crf 28 "$output"
     fi
+
+    if [[ $DRY_RUN = true ]]; then
+        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 $OUTPUT_CRF $filter "$output"
+    else
+        echo "INPUT: $INPUT, OUTPUT: $output"
+    fi     
 }
 
 entrypoint() {
-    local input="$1"
+    INPUT_ASPECT_RATIO="$(get_aspect_ratio $INPUT)"
+    INPUT_RES_Y="$(get_video_res_y $INPUT)"
+    OUTPUT_PREFIX="$(input_to_ouptut)"
 
     if [[ $CONVERT_240 = true ]]; then
-        convert-240p "$input"
+        convert-240p 
     fi
 
     if [[ $CONVERT_360 = true ]]; then
-        convert-360p "$input"
+        convert-360p
     fi
 
     if [[ $CONVERT_480 = true ]]; then
-        convert-480p "$input"
+        convert-480p
     fi
 
     if [[ $CONVERT_720 = true ]]; then
-        convert-720p "$input"
+        convert-720p
     fi
 
     if [[ $CONVERT_1080 = true ]]; then
-        convert-1080p "$input"
+        convert-1080p
     fi
 
 }
 
-INPUT="$1"
-shift
+args=$(getopt --long format:,input:,input-regexp:,output-spec:,dry-run,no-crf -o "f:i:r:o:n:h" -- "$@")
 
-while getopts ":f:" opt; do
-    case $opt in
-        f)
-            case $OPTARG in
-                240)
-                    CONVERT_240=true
+#eval set -- "$args"
+
+while [ $# -ge 1 ]; do
+        case "$1" in
+                --)
+                    # No more options left.
+                    shift
+                    break
+                   ;;
+                -i|--input)
+                    INPUT="$2"
                 ;;
-                360)
-                    CONVERT_360=true
+                --no-crf)
+                    OUTPUT_CRF=
                 ;;
-                480)
-                    CONVERT_480=true
+                -n|--dry-run)
+                    DRY_RUN=true
                 ;;
-                720)
-                    CONVERT_720=true
+                -r|--input-regexp)
+                    INPUT_REGEXP="$2"
                 ;;
-                1080)
-                    CONVERT_1080=true
+                -o|--output-spec)
+                    OUTPUT_SPEC="$2"
                 ;;
-                *)
-                    echo 'Invalid video format'
-                    exit 1
-                ;;
-            esac
-            shift
-            shift
-        ;;
-    esac
+                -f|--format)
+                    case $2 in
+                        240)
+                            CONVERT_240=true
+                        ;;
+                        360)
+                            CONVERT_360=true
+                        ;;
+                        480)
+                            CONVERT_480=true
+                        ;;
+                        720)
+                            CONVERT_720=true
+                        ;;
+                        1080)
+                            CONVERT_1080=true
+                        ;;
+                        *)
+                            echo 'Invalid video format'
+                            exit 1
+                        ;;
+                    esac
+                    shift
+                    ;;
+                -h)
+                        echo "Display some help"
+                        exit 0
+                        ;;
+        esac
+
+        shift
 done
 
-entrypoint "$INPUT"
+entrypoint "$*"

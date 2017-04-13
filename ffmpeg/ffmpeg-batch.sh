a72accfbb44181976df4d0867eff27db68f8ab10
AgentCoop <twinpeaks.agent.cooper@gmail.com>
2017-04-13 18:30:56 +0300

    ffmpeg-batch.sh

--- ffmpeg/ffmpeg-batch.sh
+++ ffmpeg/ffmpeg-batch.sh
@@ -7,10 +7,19 @@ INPUT=
 INPUT_RES_Y=
 INPUT_REGEXP=
 INPUT_ASPECT_RATIO=
+INPUT_AUDIOFORMAT=
+INPUT_AUDIOBITRATE=
 OUTPUT_SPEC=
 DRY_RUN=
 
+VIDEO_STREAM=0:0
+AUDIO_STREAM=0:1
+VIDEO_CODEC=libx264 # or copy
+AUDIO_CODEC=aac # or copy
+AUDIO_BITRATE=192
+
 PROCESSED_COUNT=
+BATCH_MODE=no
 
 is_corrupted() {
     local input="$1"
@@ -29,6 +38,7 @@ is_corrupted() {
 input_to_ouptut() {
     local output=$(echo basename "$INPUT" | awk "match(\$0, \"$INPUT_REGEXP\", m) {printf \"$OUTPUT_SPEC\", m[1],m[2],m[3],m[4],m[5],m[6]}" \
         | sed "s/\s/_/g" \
+        | sed 's/(.*)//g' \
         | sed "s/['\",\(\)]//g" \
         | sed 's/\]//g' \
         | sed 's/_-_/_/g' \
@@ -47,6 +57,16 @@ get_aspect_ratio() {
     echo $(ffprobe "$input" 2>&1 | awk 'match($0, /DAR\s+([0-9]+:[0-9]+)/, m) { print m[1] }')
 }
 
+get_audio_bitrate() {
+    local input="$1"
+    echo $(ffprobe "$input" 2>&1 | awk "match(\$0, /Stream #${AUDIO_STREAM}.*Audio.*, ([0-9]+) kb\/s.*/, m) { print m[1] }")
+}
+
+get_audio_format() {
+    local input="$1"
+    echo $(ffprobe -loglevel error -select_streams a:0 -show_entries stream=codec_name -of default=nw=1:nk=1 "$input")
+}
+
 get_video_res_y() {
     local input="$1"
 
@@ -55,181 +75,117 @@ get_video_res_y() {
     echo $resy
 }
 
-convert-240p() {
-    local output="./videos/240p/${OUTPUT_PREFIX}_240.mp4"
+transcode() {
+    local yres="$1"
 
-    if is_corrupted "$output"; then
-        rm -f "$output"
-    fi
-
-    if [[ -f $output ]]; then
-        return
-    fi
-
-    [[ ! -d ./videos/240p ]] && mkdir -p ./videos/240p
-
-    if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
-        local filter="-vf ${PADDING_FILTER}426:240"
+    if [[ $BATCH_MODE == "no" ]]; then
+        local output="${OUTPUT_PREFIX}_${yres}.mp4"
     else
-        local filter="-vf scale=426:240"
+        local output="./videos/${yres}p/${OUTPUT_PREFIX}_${yres}.mp4"
+        [[ ! -d "./videos/${yres}p" ]] && mkdir -p "./videos/${yres}p"
     fi
+
+    if [ -f "$output" ]; then
+        exit
+    fi
+
+    case $yres in
+        240)
+            if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
+                local scaling="-vf ${PADDING_FILTER}426:240"
+            else
+                local scaling="-vf scale=426:240"
+            fi
+        ;;
+        360)
+            if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
+                local filter="-vf ${PADDING_FILTER}640:360"
+            else
+                local filter="-vf scale=640:360"
+            fi
+        ;;
+        480)
+            if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
+                local scaling="-vf ${PADDING_FILTER}854:480"
+            else
+                local scaling="-vf scale=854:480"
+            fi
+        ;;
+        720)
+            if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
+                local scaling="-vf ${PADDING_FILTER}1280:720"
+            else
+                local scaling="-vf scale=1280:720"
+            fi
+        ;;
+        1080)
+            local scaling="-vf scale=1920:1080"
+        ;; 
+    esac
 
     if [[ $DRY_RUN = true ]]; then
         echo "INPUT: $INPUT"
         echo "OUTPUT: $output"
         echo
+    elif [[ $MAKE_SAMPLE = true ]]; then
+        /usr/bin/ffmpeg -t '00:32' -ss '00:05:00' -i "$INPUT" -y -map $VIDEO_STREAM -map $AUDIO_STREAM -c:v $VIDEO_CODEC -c:a $AUDIO_CODEC -b:a "$AUDIO_BITRATE"k -ac 2 -movflags faststart -strict -2 -crf 28 $scaling "sample_${yres}.mp4"
     else
-        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 -crf 28 $filter "$output"
-    fi
+        /usr/bin/ffmpeg -i "$INPUT" -map $VIDEO_STREAM -map $AUDIO_STREAM -c:v $VIDEO_CODEC -c:a $AUDIO_CODEC -b:a "$AUDIO_BITRATE"k -ac 2 -movflags faststart -strict -2 -crf 28 $scaling "$output"
+    fi    
 }
 
-convert-360p() {
-    local output="./videos/360p/${OUTPUT_PREFIX}_360.mp4"
-
-    if is_corrupted "$output"; then
-        rm -f "$output"
-    fi
-
-    if [[ -f $output ]]; then
-        return
-    fi
-
-    [[ ! -d ./videos/360p ]] && mkdir -p ./videos/360p
-
-    if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
-        local filter="-vf ${PADDING_FILTER}640:360"
-    else
-        local filter="-vf scale=640:360"
-    fi
-
-    if [[ $DRY_RUN = true ]]; then
-        echo "INPUT: $INPUT"
-        echo "OUTPUT: $output"
-        echo         
-    else
-        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 -crf 28 $filter "$output"
-    fi
-}
-
-convert-480p() {
-    local output="./videos/480p/${OUTPUT_PREFIX}_480.mp4"
-    
-    if is_corrupted "$output"; then
-        rm -f "$output"
-    fi
-
-    if [[ -f $output ]] || [[ $INPUT_RES_Y -lt 480 ]]; then
-        return
-    fi
-
-    [[ ! -d ./videos/480p ]] && mkdir -p ./videos/480p
-
-    if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
-        local filter="-vf ${PADDING_FILTER}854:480"
-    else
-        local filter="-vf scale=854:480"
-    fi
-
-    if [[ $DRY_RUN = true ]]; then
-        echo "INPUT: $INPUT"
-        echo "OUTPUT: $output"
-        echo         
-    else
-        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 -crf 28 $filter "$output"
-    fi
-}
-
-convert-720p() {
-    local output="./videos/720p/${OUTPUT_PREFIX}_720.mp4"
-
-    if is_corrupted "$output"; then
-        rm -f "$output"
-    fi
-
-    if [[ -f $output ]] || [[ $INPUT_RES_Y -lt 720 ]]; then
-        return
-    fi
-
-    [[ ! -d ./videos/720p ]] && mkdir -p ./videos/720p
-
-    if [[ $INPUT_ASPECT_RATIO != 16:9 ]]; then
-        local filter="-vf ${PADDING_FILTER}1280:720"
-    else
-        local filter="-vf scale=1280:720"
-    fi
-
-    if [[ $DRY_RUN = true ]]; then
-        echo "INPUT: $INPUT"
-        echo "OUTPUT: $output"
-        echo            
-    else
-        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 -crf 28 $filter "$output"
-    fi
-}
-
-convert-1080p() {
-    local output="./videos/1080p/${OUTPUT_PREFIX}_1080.mp4"
-
-    if is_corrupted "$output"; then
-        rm -f "$output"
-    fi
-
-    if [[ -f $output ]] || [[ $INPUT_RES_Y -lt 800 ]]; then
-        return
-    fi
-
-    [[ ! -d ./videos/1080p ]] && mkdir -p ./videos/1080p
-
-    if [[ $DRY_RUN = true ]]; then
-        echo "INPUT: $INPUT"
-        echo "OUTPUT: $output"
-        echo          
-    else
-        /usr/bin/ffmpeg -i "$INPUT" -sn -movflags faststart -strict -2 -crf 28 -vf scale=1920:1080 "$output"
-    fi
-}
 
 entrypoint() {
     INPUT_ASPECT_RATIO=$(get_aspect_ratio "$INPUT")
     INPUT_RES_Y=$(get_video_res_y "$INPUT")
-    OUTPUT_PREFIX=$(input_to_ouptut)
+    
+    if [[ $MAKE_SAMPLE != true ]]; then
+        OUTPUT_PREFIX=$(input_to_ouptut)
+    fi
+    
+    INPUT_AUDIOFORMAT=$(get_audio_format "$INPUT")
+    INPUT_AUDIOBITRATE=$(get_audio_bitrate "$INPUT")
+
+    if [[ $INPUT_AUDIOBITRATE -lt $AUDIO_BITRATE ]]; then
+        echo "Input audio bitrate ${INPUT_AUDIOBITRATE}k must be greater than specified output ${AUDIO_BITRATE}k"
+        exit
+    fi
 
     if [[  -f batch.stats ]]; then
         PROCESSED_COUNT=$(cat batch.stats | cut -f 1)
     else
         PROCESSED_COUNT=0
-    fi 
-
+    fi
 
     if [[ $CONVERT_240 = true ]]; then
-        convert-240p
+        transcode 240
         PROCESSED_COUNT=$((PROCESSED_COUNT + 1))
     fi
 
     if [[ $CONVERT_360 = true ]]; then
-        convert-360p
+        transcode 360
         PROCESSED_COUNT=$((PROCESSED_COUNT + 1))
     fi
 
     if [[ $CONVERT_480 = true ]]; then
-        convert-480p
+        transcode 480
         PROCESSED_COUNT=$((PROCESSED_COUNT + 1))
     fi
 
     if [[ $CONVERT_720 = true ]]; then
-        convert-720p
+        transcode 720
         PROCESSED_COUNT=$((PROCESSED_COUNT + 1))
     fi
 
     if [[ $CONVERT_1080 = true ]]; then
-        convert-1080p
+        transcode 1080
         PROCESSED_COUNT=$((PROCESSED_COUNT + 1))
     fi
 
     echo "$PROCESSED_COUNT" > batch.stats
 }
 
-args=$(getopt --long format:,input:,input-regexp:,output-spec:,dry-run -o "f:i:r:o:n:h" -- "$@")
+args=$(getopt --long format:,input:,input-regexp:,output-spec:,audio-stream:,video-stream:,dry-run,make-sample,vc,ac,vs,ab -o "f:i:r:o:A:V:Bn:h" -- "$@")
 
 while [ $# -ge 1 ]; do
         case "$1" in
@@ -240,6 +196,27 @@ while [ $# -ge 1 ]; do
                    ;;
                 -i|--input)
                     INPUT="$2"
+                ;;
+                -B)
+                    BATCH_MODE="yes"
+                ;;                
+                --vc)
+                    VIDEO_CODEC="$2"
+                ;;
+                -V|--video-stream)
+                    VIDEO_STREAM="$2"
+                ;;
+                --ac)
+                    AUDIO_CODEC="$2"
+                ;;
+                -A|--audio-stream)
+                    AUDIO_STREAM="$2"
+                ;;
+                --ab)
+                    AUDIO_BITRATE="$2"
+                ;;
+                --make-sample)
+                    MAKE_SAMPLE=true
                 ;;
                 -n|--dry-run)
                     DRY_RUN=true
